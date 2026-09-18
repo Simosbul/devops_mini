0c91b11d31d6811dac48583db24db593b2710bd3
Simosbul <Simeongsb@gmail.com>
2026-09-18 16:24:22 +0300

    Updated VPC.tf file

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -76,3 +76,47 @@ resource "aws_route_table_association" "devops_mini_public_2" {
   subnet_id      = aws_subnet.devops_mini_public_2.id
   route_table_id = aws_route_table.devops_mini_public.id
 }
+
+resource "aws_eip" "devops_mini_nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "devops-mini-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "devops_mini" {
+  allocation_id = aws_eip.devops_mini_nat.id
+  subnet_id     = aws_subnet.devops_mini_public_1.id
+
+  tags = {
+    Name = "devops-mini-nat"
+  }
+
+  depends_on = [
+    aws_internet_gateway.devops_mini
+  ]
+}
+
+resource "aws_route_table" "devops_mini_private" {
+  vpc_id = aws_vpc.devops_mini.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.devops_mini.id
+  }
+
+  tags = {
+    Name = "devops-mini-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "devops_mini_private_1" {
+  subnet_id      = aws_subnet.devops_mini_private_1.id
+  route_table_id = aws_route_table.devops_mini_private.id
+}
+
+resource "aws_route_table_association" "devops_mini_private_2" {
+  subnet_id      = aws_subnet.devops_mini_private_2.id
+  route_table_id = aws_route_table.devops_mini_private.id
+}
