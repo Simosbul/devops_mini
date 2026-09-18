555abbbf3f4faab61c641733e5d949c08b9ae96e
Simosbul <Simeongsb@gmail.com>
2026-09-18 16:23:08 +0300

    Updated Node-Group.TF

--- terraform/Node-group.tf
+++ terraform/Node-group.tf
@@ -15,4 +15,12 @@ resource "aws_eks_node_group" "devops_mini" {
   }
 
   instance_types = ["t3.small"]
+
+  depends_on = [
+    aws_eip.devops_mini_nat,
+    aws_nat_gateway.devops_mini,
+    aws_route_table.devops_mini_private,
+    aws_route_table_association.devops_mini_private_1,
+    aws_route_table_association.devops_mini_private_2
+  ]
 }
