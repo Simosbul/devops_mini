cccfec0770e73faf2464dbd8cb3db961f8a0bfca
Simosbul <Simeongsb@gmail.com>
2026-09-23 19:53:37 +0300

    Updated s3bucket.tf file

--- terraform/s3bucket.tf
+++ terraform/s3bucket.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "devops_mini" {
-  bucket = "devops-mini-${data.aws_caller_identity.current.account_id}"
+  bucket        = "devops-mini-${data.aws_caller_identity.current.account_id}"
+  force_destroy = true
 }
 
 data "aws_caller_identity" "current" {}
